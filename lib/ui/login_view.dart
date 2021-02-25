fc31b1aa648693f4f1ba13b223d90ff83103d565
Robert Pierson <roberthpierson@yahoo.com>
2021-02-25 07:23:03 -0800

    Improvements to textfields for email/passwords to obscure text and provide a better experience.

--- lib/ui/login_view.dart
+++ lib/ui/login_view.dart
@@ -13,100 +13,105 @@ class LoginView extends StatelessWidget {
       child: Consumer<LoginViewModel>(
         builder: (context, model, child) => SafeArea(
           child: Scaffold(
-            resizeToAvoidBottomInset: false,
             backgroundColor: kColorSkylineGreen,
-            body: Column(
-              children: [
-                Container(child: IconButton(icon: Icon(Icons.arrow_back,color: kColorSkylineWhite,), onPressed: ()=> model.routeToHomeView()),alignment: Alignment.topLeft,),
-                SizedBox(
-                  height: 140.0,
-                ),
-                Container(
-                  child: Center(child: Text("Skyline Flutter Login View",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: kColorSkylineWhite),)),
-                ),SizedBox(height: 10,),
-                Container(child: Image.asset('assets/SkyLineLogo.png'),),
-                SizedBox(
-                  height: 18.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: TextField(
-                    onChanged: (value) {
-                      //Do something with the user input.
-                    },
-                    decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
-                      hintText: 'Enter your email',
-                      contentPadding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                    ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(child: IconButton(icon: Icon(Icons.arrow_back,color: kColorSkylineWhite,), onPressed: ()=> model.routeToHomeView()),alignment: Alignment.topLeft,),
+                  SizedBox(
+                    height: 140.0,
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: TextField(
-                    onChanged: (value) {
-                      //Do something with the user input.
-                    },
-                    decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
-                      hintText: 'Enter your password.',
-                      contentPadding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                    ),
+                  Container(
+                    child: Center(child: Text("Skyline Flutter Login View",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: kColorSkylineWhite),)),
+                  ),SizedBox(height: 10,),
+                  Container(child: Image.asset('assets/SkyLineLogo.png'),),
+                  SizedBox(
+                    height: 18.0,
                   ),
-                ),
-                SizedBox(
-                  height: 24.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 2.0),
-                  child: Material(
-                    color: kColorSkyLineGrey,
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      onPressed: () {
-                        //Implement login functionality.
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value) {
+                        //Do something with the user input.
                       },
-                      minWidth: 330.0,
-                      height: 42.0,
-                      child: Text(
-                        'Log In',
+                      decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
+                        hintText: 'Enter your email',
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                      onChanged: (value) {
+                        //Do something with the user input.
+                      },
+                      decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
+                        hintText: 'Enter your password.',
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 2.0),
+                    child: Material(
+                      color: kColorSkyLineGrey,
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                          //Implement login functionality.
+                        },
+                        minWidth: 330.0,
+                        height: 42.0,
+                        child: Text(
+                          'Log In',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+              ),
             ),
           ),
         ),
--- lib/ui/registration_view.dart
+++ lib/ui/registration_view.dart
@@ -13,101 +13,106 @@ class RegistrationView extends StatelessWidget {
       child: Consumer<RegistrationViewModel>(
         builder: (context, model, child) => SafeArea(
           child: Scaffold(
-            resizeToAvoidBottomInset: false,
             backgroundColor: kColorSkylineGreen,
-            body: Column(
-              children: [
-                Container(child: IconButton(icon: Icon(Icons.arrow_back,color: kColorSkylineWhite,), onPressed: ()=> model.routeToHomeView()),alignment: Alignment.topLeft,),
-                SizedBox(
-                  height: 140.0,
-                ),
-                Container(
-                  child: Center(child: Text("Skyline Flutter Registration View",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: kColorSkylineWhite),)),
-                ),SizedBox(height: 10,),
-                Container(child: Image.asset('assets/SkyLineLogo.png'),),
-                SizedBox(
-                  height: 18.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: TextField(
-                    onChanged: (value) {
-                      model.setEmailAddress(value);
-                    },
-                    decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
-                      hintText: 'Enter your email',
-                      contentPadding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                    ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(child: IconButton(icon: Icon(Icons.arrow_back,color: kColorSkylineWhite,), onPressed: ()=> model.routeToHomeView()),alignment: Alignment.topLeft,),
+                  SizedBox(
+                    height: 140.0,
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: TextField(
-                    onChanged: (value) {
-                      model.setPassword(value);
-                    },
-                    decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
-                      hintText: 'Enter your password.',
-                      contentPadding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 1.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                        BorderSide(color: kColorSkyLineGrey, width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                      ),
-                    ),
+                  Container(
+                    child: Center(child: Text("Skyline Flutter Registration View",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: kColorSkylineWhite),)),
+                  ),SizedBox(height: 10,),
+                  Container(child: Image.asset('assets/SkyLineLogo.png'),),
+                  SizedBox(
+                    height: 18.0,
                   ),
-                ),
-                SizedBox(
-                  height: 24.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 2.0),
-                  child: Material(
-                    color: kColorSkyLineGrey,
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      onPressed: () {
-                       print(model.email);
-                       print(model.password);
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value) {
+                        model.setEmailAddress(value);
                       },
-                      minWidth: 330.0,
-                      height: 42.0,
-                      child: Text(
-                        'Register',
+                      decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
+                        hintText: 'Enter your email',
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                      onChanged: (value) {
+                        model.setPassword(value);
+                      },
+                      decoration: InputDecoration(filled: true,fillColor: kColorSkylineWhite,
+                        hintText: 'Enter your password.',
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                          BorderSide(color: kColorSkyLineGrey, width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 2.0),
+                    child: Material(
+                      color: kColorSkyLineGrey,
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                         print(model.email);
+                         print(model.password);
+                        },
+                        minWidth: 330.0,
+                        height: 42.0,
+                        child: Text(
+                          'Register',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+              ),
             ),
           ),
         ),
